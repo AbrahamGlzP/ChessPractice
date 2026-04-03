56396523e2208cb47dd26e11bbb5a1aa1e3076bb
AbrahamGlzP <abrahamglzp@gmail.com>
2026-04-03 13:12:19 -0600

    Initial Commit

---
+++ ChessPractice/ChessPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChessPracticeApp.swift
+//  ChessPractice
+//
+//  Created by Abraham Gonzalez Puga on 03/04/26.
+//
+
+import SwiftUI
+
+@main
+struct ChessPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ChessPractice/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ChessPractice
+//
+//  Created by Abraham Gonzalez Puga on 03/04/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
